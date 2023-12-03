742b344bc336b70c9b887a41fe238dd54515eed9
Suravi <suraviroy1509@gmail.com>
2023-12-03 18:51:22 +0530

    changed code

--- src/pages/Treee.js
+++ src/pages/Treee.js
@@ -1,22 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import './Treee.css';
 
-import React, { useState , useEffect  } from 'react';
-import './Treee.css'; 
-
-const TreeNode = ({ value, children }) => {
+const TreeNode = ({ value, children, side }) => {
   return (
-    <div className="tree-node">
+    <div className={`tree-node ${side}`}>
       {value}
       <div className="children">{children}</div>
     </div>
   );
 };
 
-const Tree = ({ tree }) => {
+const Tree = ({ tree, side = 'center' }) => {
   const [treeState, setTree] = useState(tree);
 
-  const [preOrderResult, setPreOrderResult] = useState([]);
-  const [inOrderResult, setInOrderResult] = useState([]);
-  const [postOrderResult, setPostOrderResult] = useState([]);
+
+  // const [preOrderResult, setPreOrderResult] = useState([]);
+  // const [inOrderResult, setInOrderResult] = useState([]);
+  // const [postOrderResult, setPostOrderResult] = useState([]);
 
   useEffect(() => {
     if (!treeState) {
@@ -44,52 +44,48 @@ const Tree = ({ tree }) => {
   };
 
 
-  const traverseTree = (order, node) => {
-    if (node) {
-      if (order === 'pre') setPreOrderResult((prev) => [...prev, node.value]);
-      traverseTree(order, node.left);
-      if (order === 'in') setInOrderResult((prev) => [...prev, node.value]);
-      traverseTree(order, node.right);
-      if (order === 'post') setPostOrderResult((prev) => [...prev, node.value]);
-    }
-  };
- 
-  useEffect(() => {
-    traverseTree('pre', treeState);
-    traverseTree('in', treeState);
-    traverseTree('post', treeState);
-  }, [treeState]);
+
+  // const traverseTree = (order, node) => {
+  //   if (node) {
+  //     if (order === 'pre') setPreOrderResult((prev) => [...prev, node.value]);
+  //     traverseTree(order, node.left);
+  //     if (order === 'in') setInOrderResult((prev) => [...prev, node.value]);
+  //     traverseTree(order, node.right);
+  //     if (order === 'post') setPostOrderResult((prev) => [...prev, node.value]);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   traverseTree('pre', treeState);
+  //   traverseTree('in', treeState);
+  //   traverseTree('post', treeState);
+  // }, [treeState]);
+
 
   return (
- 
+
     <div className="tree-container">
-     
+
       {treeState && (
-        <TreeNode value={treeState.value}>
-          {treeState.left && <Tree tree={treeState.left} />}
-          {treeState.right && <Tree tree={treeState.right} />}
+        <TreeNode value={treeState.value} side={side}>
+          {treeState.left && <Tree tree={treeState.left} side="left" />}
+          {treeState.right && <Tree tree={treeState.right} side="right" />}
         </TreeNode>
       )}
       <div className="buttons">
-        {/* <button className="addButton" onClick={() => addNode('left')}>
-          Add Left Node
-        </button> */}
         {(treeState && !treeState.left) && (
-        <button className="addButton" onClick={() => addNode('left')}>
-          Add Left of {treeState.value}
-        </button>
-      )}
-        {/* <button className="addButton" onClick={() => addNode('right')}>
-          Add Right Node
-        </button> */}
+          <button className="addButton" onClick={() => addNode('left')}>
+            Add Left of {treeState.value}
+          </button>
+        )}
         {(treeState && !treeState.right) && (
-        <button className="addButton" onClick={() => addNode('right')}>
-          Add Right of {treeState.value}
-        </button>
-      )}
+          <button className="addButton" onClick={() => addNode('right')}>
+            Add Right of {treeState.value}
+          </button>
+        )}
       </div>
 
-       {/* Display traversal results at the bottom of the page */}
+     
       {/* <div className="traversal-results">
       
         <div>
@@ -104,10 +100,12 @@ const Tree = ({ tree }) => {
         </div> */}
 
     </div>
-   
+
   );
 };
 
 export default Tree;
+
+
 
 
